d184c60f2f54642ca16d70438ebd27efec11110d
Fariba Shamseh <fariba.shamseh0@gmail.com>
2024-08-26 02:30:42 +0430

    components composition and reusability

--- src/App.jsx
+++ src/App.jsx
@@ -35,11 +35,22 @@ export default function App() {
         </div>
     );
 }
-function TextExpander(){
-    return(
-        <div>
-            <span>test</span>
-            <button>show more</button>
+function TextExpander({children,collapsedNumWords=20,expandButtonText="show more",collapseButtonText="show less  ",buttonColor="#1f09cd",expanded= false,className}){
+    const [isExpanded , setIsExpanded]=useState(expanded);
+    const displayText= isExpanded ? children : children.split('').slice(0,collapsedNumWords).join('')+"...";
+    const buttonStyle= {
+        background:'none',
+        border:'none',
+        font:'inherit',
+        cursor:'pointer',
+        marginLeft:'6px',
+        color:buttonColor
+    };
+    return (
+        <div className={className}>
+            <span>{displayText}</span>
+            <button onClick={() => setIsExpanded((exp) => !exp)}
+                    style={buttonStyle}>{isExpanded ? collapseButtonText : expandButtonText}</button>
         </div>
     )
 }
